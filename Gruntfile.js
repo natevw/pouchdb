84e8d966c1e798f8d91389fc8529cfc67538af38
Nathan Vander Wilt <nate@calftrail.com>
2013-03-08 15:25:26 -0800

    starting to implement

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ var http_proxy = require("http-proxy");
 var srcFiles = [
   "src/pouch.js", "src/pouch.collate.js", "src/pouch.merge.js",
   "src/pouch.replicate.js", "src/pouch.utils.js", "src/pouch.adapter.js",
-  "src/adapters/pouch.http.js", "src/adapters/pouch.idb.js",
+  "src/adapters/pouch.http.js", "src/adapters/pouch.idb.js", "src/adapters/pouch.webrtc.js",
   "src/adapters/pouch.websql.js", "src/plugins/pouchdb.mapreduce.js"
 ];
 
